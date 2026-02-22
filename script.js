dc503143f875aac1157a70fb08d7551462b46edc
Jana Schroth <schroth.jana@gmail.com>
2026-02-22 10:29:45 -0800

    Assesment01_complete

--- script.js
+++ script.js
@@ -65,33 +65,33 @@ Using a set of Comparison and Logical operators, write an application that evalu
 Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result within the console window. The application cannot use the same operators used in the previous application.
 */
 
-/* ANSWERS
+//ANSWERS
 
 //STEP1
-
+/*
 let someMonth
 function theMonth()
 let currentMonth
 let myLibraryFunction
-
+*/
 
 //STEP2
-
+/*
 32 //(numeric)
 'This is a string literal'
 true //(boolean)
 null
-
+*/
 
 //STEP3
-
+/*
 let travelDestination = 'Hawaii'
 let devisionRemainder = 933%2
 console.log (devisionRemainder)
-
+*/
 
 //STEP4
-
+/*
 let firstName
 let lastName
 let address
@@ -101,50 +101,50 @@ let zipCode
 let yourAge
 let referralSource
 let blnPermToContact
-
+*/
 
 //STEP5
-
-
+/*
 let firstName 
 firstName = prompt ('What is your first name?')
 
 let blnPermToContact = false
 
 let firstName = 'John', blnPermToContact = false
-
+*/
 
 //STEP6
-
+/*
 let result = 5 + ' minutes'
 console.log (result)
-
+*/
 
 //STEP7
-
-
+/*
 let firstVar = 'Is it Friday today?: ' + false
 console.log (firstVar)
 
 let secondVar = 5 + true
 console.log (secondVarVar)
-
+*/
 
 //STEP8
 //add a string escape after "I"
+/*
 let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
 console.log(someString)
-
+*/
 
 //STEP9
-
+/*
 let nullResult = null
 console.log (nullResult)
 let undefinedResult
 console.log (undefinedResult)
-
+*/
 
 //STEP10
+/*
 let message = 'This is a string literal'
 console.log (typeof message)
 let age = 32
@@ -155,85 +155,100 @@ let fullName = {firstName:'Jana', lastName:'Schroth'}
 console.log(typeof fullName)
 let x
 console.log(typeof x)
+*/
 
 //STEP11
-
+/*
 alert ('Hello ' + 'Jana Schroth' + ', welcome to the JavaScript class!')
+*/
 
 //STEP12
-
+/*
 let Name = 'Jana Schroth'
 alert ('Hello ' + Name + ', welcome to the JavaScript class!')
+*/
 
 //STEP13
-
+/*
 let Name = 'Jana Schroth'
 let course = 'JavaScript'
 alert ('Hello ' + Name + `Welcome to the ${course} class!`)
+*/
 
 //STEP14
-
+/*
 let Name = 'Jana Schroth'
 let course = 'JavaScript'
 alert ('Hello ' + Name + `\nWelcome to the ${course} class!`)
+*/
 
 //STEP15
-
+/*
 let Name = prompt ('Enter your first and last name:')
 let course = 'JavaScript'
 alert ('Hello ' + Name + `\nWelcome to the ${course} class!`)
+*/
 
 //STEP16
-
+/*
 let Name = prompt ('Enter your first and last name:')
 let course = prompt ('Enter the course name:')
 alert ('Hello ' + Name + `\nWelcome to the ${course} class!`)
+*/
 
 //STEP17
+/*
 let x = 10
 let y = 20
 let sum = x + y
 console.log(sum)
+*/
 
 //STEP18
-
+/*
 let x = 20
 x = x + 20
 console.log(x)
+*/
 
 //STEP19
-
+/*
 let x = 20
 x = x * 5
 console.log(x)
+*/
 
 //STEP20
-
+/*
 let x = 20 % 3
 x = x / 1
 console.log(x)
+*/
 
 //STEP21
-
+/*
 let miles = 300
 let gallons = 20
 let MPG = miles / gallons
 let result = MPG <= 25
 alert (`Your car get less that 25 MPG on a full tank of gas : ${result}`)
+*/
 
 //STEP22
-
+/*
 let heightPerson1 = 75
 let heightPerson2 = 45
 let result = heightPerson1 == heightPerson2
 alert (`Both individuals have the same height: ${result}`)
+*/
+
 
 //extra
+/*
 let namePerson1 = prompt ('Enter name for person 1:')
 let heightPerson1 = prompt ('Enter height in inch for person 1:')
 let namePerson2 = prompt ('Enter name for person 2:')
 let heightPerson2 = prompt ('Enter height in inch for person 2:')
 let result = heightPerson1 > heightPerson2
 alert (`${namePerson1} is taller than ${namePerson2}: ${result}`)
-
 */
